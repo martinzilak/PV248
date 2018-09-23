fa133d73855fd52aae15282359868453715e982b
tinnyx <tinnyx@gmail.com>
2018-09-23 12:42:24 +0200

    final version

--- 01-data_parsing/stat.py
+++ 01-data_parsing/stat.py
@@ -6,13 +6,14 @@ import codecs
 from collections import Counter
 
 MODES = ['composer', 'century']
+YEAR_FORMATS = [r'\w*\s*Year:\s(\d{3,4}).*\n', r'\w*\s*Year:\s(\d*)\w{2}\scentury\n', r'\w*\s*Year:\s[a-z.]{2,3}\s(\d{3,4}).*\n']
 
 if len(sys.argv) != 3:
     raise ValueError('Wrong number of arguments passed')
 if sys.argv[2] not in MODES:
     raise ValueError('Unknown mode passed, possible values are: ', MODES)
 
-f = codecs.open(sys.argv[1], 'r', encoding = "cp1250", errors='ignore')
+file = codecs.open(sys.argv[1], 'r', encoding = "cp1250", errors='ignore')
 mode = sys.argv[2]
 
 ctr = Counter()
@@ -24,25 +25,34 @@ def century(year):
     c = year // 100 + 1
     return(suf(c) + ' century')
 
-for line in f:
+if mode == 'composer':
+    rcomposer = re.compile(r'Composer:\s(.*)\n')
+    ryear = re.compile(r'(.*?)\(.*?\)')
+
+if mode == 'century':
+    ryf = []
+    for yf in YEAR_FORMATS:
+        ryf.append(re.compile(yf))
+
+for line in file:
     if mode == 'composer':
-        rcomposer = re.compile(r"Composer:\s(.*)\n")
-        ryear = re.compile(r"(.*?)\(.*?\)")
         mcomposer = rcomposer.match(line)
         if mcomposer:
             for name in mcomposer.group(1).split(';'):
                 myear = ryear.match(name)
-                if myear:
-                    ctr[myear.group(1).strip()] += 1
-                else:
-                    ctr[name.strip()] += 1
+                n = myear.group(1) if myear else name
+                n = n.strip()
+                if len(n) > 0:
+                    ctr[n] += 1
     if mode == 'century':
-        r = re.compile(r"\sYear:\s(\d*)\n")
-        m = r.match(line)
-        if m:
-            y = append(m.group(1).strip())
-            if len(y) > 0:
-                ctr[century(int(y))] += 1
+        for r in ryf:
+            m = r.match(line)
+            if m:
+                y = m.group(1).strip()
+                if len(y) > 2:
+                    ctr[century(int(y))] += 1
+                elif len(y) > 0:
+                    ctr[suf(int(y)) + ' century'] += 1
 
 for k, v in ctr.items():
     print(k, ': ', v, sep = '')
@@ -50,12 +60,17 @@ for k, v in ctr.items():
 '''
 found year formats:
 XXXX
-XXXX/XXXX
-XXth century
 XXXX Leipzig
-ca XXXX
-ca. XXXX
+XXXX/XXXX
 XXXX--XXXX
 XXXX, Hamburg
+\w*\s*Year:\s(\d{3,4}).*\n
+
+XXth century
+\w*\s*Year:\s(\d*)\w{2}\scentury\n
+
+ca XXXX
+ca. XXXX
+\w*\s*Year:\s[a-z.]{2,3}\s(\d{3,4}).*\n
 '''
 
